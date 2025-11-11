7491f8772a09e29eb2fea39af9a3b3c503d7a010
Mark Melikov <markmelicov@gmail.com>
2025-11-11 09:28:38 +0200

    https://github.com/cs-intro-reichman/hw2-Sokol1001.git

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,44 @@
 // Demonstrates the Collatz conjecture.
-public class Collatz {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
-	}
+public class Collatz 
+{
+    public static void main(String[] args) {
+
+        int maxSeed = Integer.parseInt(args[0]);
+        String mode = args[1];
+
+        int currentSeed = 1;
+        while (currentSeed <= maxSeed) {
+            
+            long currentNumber = currentSeed;
+            int stepCount = 0;
+            String sequenceOutput = "";
+
+            if (mode.equals("v")) {
+                sequenceOutput = sequenceOutput + currentNumber;
+            }
+
+            while (currentNumber != 1) {
+                
+                if (currentNumber % 2 == 0) {
+                    currentNumber = currentNumber / 2;
+                } else {
+                    currentNumber = currentNumber * 3 + 1;
+                }
+                
+                stepCount = stepCount + 1;
+
+                if (mode.equals("v")) {
+                    sequenceOutput = sequenceOutput + " " + currentNumber;
+                }
+            }
+
+            if (mode.equals("v")) {
+                System.out.println(sequenceOutput + " (" + stepCount + ")");
+            }
+            
+            currentSeed = currentSeed + 1;
+        }
+
+        System.out.println("Every one of the first " + maxSeed + " hailstone sequences reached 1.");
+    }
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,31 @@
 public class TimeCalc {
     public static void main(String[] args) {
         // Replace this comment with your code
+        String currentTime = args[0];
+        int inputMinutes = Integer.parseInt(args[1]);
+
+        String currentTimeArr[] = currentTime.split(":");
+        int currentHour = Integer.parseInt(currentTimeArr[0]);
+        int currentMinutes = Integer.parseInt(currentTimeArr[1]);
+        
+        int totalMin = (currentHour * 60) + currentMinutes + inputMinutes;
+        int totalHours = totalMin / 60;
+        int newHours = totalHours % 24;
+        int newMinutes = totalMin - (totalHours * 60);
+
+        if(newHours < 10 && newMinutes > 10){
+            System.out.println("0" + newHours + ":" + newMinutes);
+        }
+        else if(newMinutes < 10 && newHours > 10){
+            System.out.println(newHours + ":" + "0" + newMinutes);
+        }
+        else if(newHours < 10 && newMinutes < 10){
+            System.out.println("0" + newHours + ":" + "0" + newMinutes);
+        }
+        else{
+            System.out.println(newHours + ":" + newMinutes);
+        }
+        System.out.println("newMinutes "+ newMinutes);
+        System.out.println("newHours "+ newHours);
     }
 }
